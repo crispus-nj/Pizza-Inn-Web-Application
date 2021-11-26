709a451cdc8cffa391c411e02518c341292befc4
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-26 22:44:11 +0300

    created class Pizza in and initialised its paremeter to four arguements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// 
+// class Pizza
+class Pizza {
+  constructor(customerName, pizzaSize, delivery = Boolean, price = 0) {
+    this.customerName = customerName;
+    this.pizzaSize = pizzaSize;
+    this.delivery = delivery;
+    this.price = price;
+  }
+}
