4612d96ad3fe2a5bab18989ef68c0014adead080
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-28 11:41:19 +0300

    declared global variables. added an event listener to the btn to output the result from the to the console

--- js/index.js
+++ js/index.js
@@ -1,48 +1,74 @@
 // class Pizza
+// global variables
+let nameCustomer = document.getElementById("name");
+let emailCustomer = document.getElementById("email");
+let sizePizza = document.getElementById("quantity");
+let deliveryItems = document.getElementById("delivery");
+let btn = document.querySelector(".btn-danger");
+let price = document.getElementsByClassName("price");
+let crustPizza = document.getElementById('crust')
+// console.log(price)
+
+// globa variable with values from the input fields
+
 class Pizza {
-  constructor(customerName, customerEmail,pizzaSize, delivery = Boolean, price = 0) {
+  constructor(
+    customerName,
+    customerEmail,
+    pizzaSize,
+    pizzaCrust,
+    delivery = Boolean,
+    price = 0
+  ) {
     this.customerName = customerName;
-    this.customerEmail = customerEmail
+    this.customerEmail = customerEmail;
     this.pizzaSize = pizzaSize;
+    this.pizzaCrust = pizzaCrust;
     this.delivery = delivery;
     this.price = price;
   }
   getTotalPrice(crust, toppings, quantity) {
     if (this.pizzaSize == "small") {
-      if(this.delivery == true){
-        return (this.price += crust + toppings + 300 + 200) * quantity
+      if (this.delivery == true) {
+        return (this.price += crust + toppings + 300 + 200) * quantity;
       } else {
-        return (this.price += crust + toppings + 300) * quantity
+        return (this.price += crust + toppings + 300) * quantity;
       }
-    }
-    else if (this.pizzaSize == "medium"){
-      if(this.delivery == true){ 
-        return (this.price += crust + toppings + 500 + 200) * quantity
-      }else {
-        return (this.price += crust + toppings + 500) * quantity
+    } else if (this.pizzaSize == "medium") {
+      if (this.delivery == true) {
+        return (this.price += crust + toppings + 500 + 200) * quantity;
+      } else {
+        return (this.price += crust + toppings + 500) * quantity;
       }
-    }else {
-      if(this.delivery == true){
-        return (this.price += crust + toppings + 800 + 200) * quantity
-      }else {
-        return (this.price += crust + toppings + 800) * quantity
+    } else {
+      if (this.delivery == true) {
+        return (this.price += crust + toppings + 800 + 200) * quantity;
+      } else {
+        return (this.price += crust + toppings + 800) * quantity;
       }
     }
   }
-  quantityChange(event){
-    let inputNum = document.getElementById('numQuantity')
-    let input = inputNum.value
-    input = event.target
+  quantityChange(event) {
+    let inputNum = document.getElementById("numQuantity");
+    let input = inputNum.value;
+    input = event.target;
     // event.target
-    if(isNaN(input) || input <= 0){
-      input = 1
+    if (isNaN(input) || input <= 0) {
+      input = 1;
     }
-    this.getTotalPrice()
+    this.getTotalPrice();
   }
 }
-
-let pizza = new Pizza("Crispus Njenga","engineer@gmail.com" ,"medium", true);
-console.log(`Customer Name: ${pizza.customerName}
-customer Email: ${pizza.customerEmail}
-Pizza Size: ${pizza.pizzaSize}
-Total Price: ${pizza.getTotalPrice(200, 100, 2)}`);
+btn.addEventListener("click", function () {
+  let name = nameCustomer.value;
+  let email = emailCustomer.value;
+  let size = sizePizza.value;
+  let crust = crustPizza.value
+  let deliveryitem = deliveryItems.value;
+  let pizza = new Pizza(name, email, size, crust ,deliveryitem);
+  console.log(`Customer Name: ${pizza.customerName}
+  customer Email: ${pizza.customerEmail}
+  Pizza Size: ${pizza.pizzaSize}
+  Pizza Crust: ${pizza.pizzaCrust}
+  Total Price: ${pizza.getTotalPrice(200, 100, 2)}`);
+});
