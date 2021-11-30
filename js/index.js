1f3134fa6d7ed7623a9ba7e8efdfc237c3d664ff
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-30 11:40:32 +0300

    added the customer review and services section, styled the images inside this sections and flexbox functionalities inside there

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ let price = document.getElementsByClassName("price");
 let crustPizza = document.getElementById("crust");
 let numQuantityItems = document.getElementById('numQuantity')
 let display = document.getElementById("confirm")
+let totalPricePizza = document.getElementById('totalPrice')
 // console.log(price)
 
 // globa variable with values from the input fields
@@ -32,21 +33,21 @@ class Pizza {
   getTotalPrice(crust, toppings, quantity) {
     if (this.pizzaSize == "small") {
       if (this.delivery == true) {
-        return (this.price += crust + toppings + 300 + 200 -300) * quantity;
+        return (this.price += crust + toppings * quantity + 300 + 200);
       } else {
-        return (this.price += crust + toppings + 300 - 300) * quantity;
+        return (this.price += crust + toppings + 300 )* quantity;
       }
     } else if (this.pizzaSize == "medium") {
       if (this.delivery == true) {
-        return (this.price += crust + toppings + 500 + 200 - 500) * quantity;
+        return (this.price += crust + toppings + 500 + 200 ) * quantity;
       } else {
-        return (this.price += crust + toppings + 500 - 500) * quantity;
+        return (this.price += crust + toppings + 500 ) * quantity;
       }
     } else {
       if (this.delivery == true) {
-        return (this.price += crust + toppings + 800 + 200 - 800) * quantity;
+        return (this.price += crust + toppings + 800 + 200) * quantity;
       } else {
-        return (this.price += crust + toppings + 800 - 800) * quantity;
+        return (this.price += crust + toppings + 800) * quantity;
       }
     }
   }
@@ -69,7 +70,9 @@ btn.addEventListener("click", function () {
   let crust = crustPizza.value;
   let deliveryitem = deliveryItems.checked;
   let numQuantityItem = numQuantityItems.value
-  console.log(display)
+
+  console.log(numQuantityItem)
+  // console.log(display)
   let pizza = new Pizza(name, email, size, crust, deliveryitem);
   // output
   console.log(`Customer Name: ${pizza.customerName}
@@ -113,18 +116,30 @@ btn.addEventListener("click", function () {
               </div>
               <div class="row d-flex justify-content-center align-items-center mt-3">
                   <div class="col-md-4">
-                      <h4><span style="font-weight: bold; color: brown;">Total price:</span>${pizza.getTotalPrice(200, 100, numQuantityItem)}</h4>
+                      <h4><span style="font-weight: bold; color: brown;">Total price:</span>Ksh ${pizza.getTotalPrice(200, 100, numQuantityItem)}</h4>
                   </div>
               </div>
 
               <div class="modal-buttons d-flex justify-content-center align-items-center mt-3" id="confirm-buttons">
-                  <button type="submit" class="btn btn-success button-confirm">Confirm</button>
+                  <button type="submit" class="btn btn-success button-confirm" id="confirmOrder">Confirm</button>
                   <button type="submit" class="btn btn-danger" data-dismiss="modal">Cancel</button>
               </div>
           </div>
       </div>
   </div>
 </div>`
+// totalPricePizza.innerHTML = pizza.getTotalPrice(200, 100, numQuantityItem)
   // console.log(pizza.quantityChange(numQuantityItem))
 });
+
+$(document).ready(function(){
+  let orderPizza = new Pizza(name, email, size, crust, deliveryitem)
+  $('#confirmOrder').click(function(){
+    alert(`Pizza Size: ${orderPizza.size}
+    Pizza Crust: ${orderPizza.crust}
+    Delivery: ${orderPizza.deliveryitem}
+    Total Price: ${orderPizza.getTotalPrice(200, 100, numQuantityItem)}
+    `)
+  })
+})
 
